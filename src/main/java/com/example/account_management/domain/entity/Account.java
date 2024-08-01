fa3acd5d3e1cf2e899a1103a17b9a59cdb2b5a2e
Aliaksandr <dyvedd@gmail.com>
2024-08-01 23:08:30 +0300

    Add entity, repository, service

---
+++ src/main/java/com/example/account_management/domain/entity/Account.java
@@ -0,0 +1,53 @@
+package com.example.account_management.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal balance;
+    private boolean isBlocked;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        isBlocked = blocked;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
